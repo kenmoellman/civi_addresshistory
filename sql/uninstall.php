2ec2bde212b826e5504632daa703429d271cf800
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-07-21 03:25:02 -0400

    Update uninstall.php

--- sql/uninstall.php
+++ sql/uninstall.php
@@ -1,6 +1,10 @@
 -- Uninstall script for address history extension
 -- File: sql/uninstall.sql
 
--- Drop triggers (will be done by upgrader)
+-- Drop triggers first
+DROP TRIGGER IF EXISTS civicrm_address_history_after_insert;
+DROP TRIGGER IF EXISTS civicrm_address_history_after_update;
+DROP TRIGGER IF EXISTS civicrm_address_history_after_delete;
+
 -- Drop the address history table
 DROP TABLE IF EXISTS `civicrm_address_history`;
