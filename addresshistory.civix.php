8a7e2b1c9a5c9c3a9006c337d6a87a2bdb388900
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-07-21 00:33:22 -0400

    Update addresshistory.civix.php

--- addresshistory.civix.php
+++ addresshistory.civix.php
@@ -13,16 +13,6 @@ class CRM_Addresshistory_ExtensionUtil {
 
   /**
    * Translate a string using the extension's domain.
-   *
-   * If the extension doesn't have a specific translation
-   * for the string, fallback to the default translations.
-   *
-   * @param string $text
-   *   Canonical message text (generally en_US).
-   * @param array $params
-   * @return string
-   *   Translated text.
-   * @see ts
    */
   public static function ts($text, $params = []) {
     if (!array_key_exists('domain', $params)) {
@@ -33,13 +23,6 @@ class CRM_Addresshistory_ExtensionUtil {
 
   /**
    * Get the URL of a resource file (in this extension).
-   *
-   * @param string|NULL $file
-   *   Ex: NULL.
-   *   Ex: 'css/foo.css'.
-   * @return string
-   *   Ex: 'http://example.org/sites/default/ext/org.example.foo'.
-   *   Ex: 'http://example.org/sites/default/ext/org.example.foo/css/foo.css'.
    */
   public static function url($file = NULL) {
     if ($file === NULL) {
@@ -50,39 +33,23 @@ class CRM_Addresshistory_ExtensionUtil {
 
   /**
    * Get the path of a resource file (in this extension).
-   *
-   * @param string|NULL $file
-   *   Ex: NULL.
-   *   Ex: 'css/foo.css'.
-   * @return string
-   *   Ex: '/var/www/example.org/sites/default/ext/org.example.foo'.
-   *   Ex: '/var/www/example.org/sites/default/ext/org.example.foo/css/foo.css'.
    */
   public static function path($file = NULL) {
-    // return CRM_Core_Resources::singleton()->getPath(self::LONG_NAME, $file);
     return __DIR__ . ($file === NULL ? '' : (DIRECTORY_SEPARATOR . $file));
   }
 
   /**
    * Get the name of a class within this extension.
-   *
-   * @param string $suffix
-   *   Ex: 'Page_HelloWorld' or 'Page\\HelloWorld'.
-   * @return string
-   *   Ex: 'CRM_Foo_Page_HelloWorld'.
    */
   public static function findClass($suffix) {
     return self::CLASS_PREFIX . '_' . str_replace('\\', '_', $suffix);
   }
-
 }
 
 use CRM_Addresshistory_ExtensionUtil as E;
 
 /**
  * (Delegated) Implements hook_civicrm_config().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config
  */
 function _addresshistory_civix_civicrm_config(&$config = NULL) {
   static $configured = FALSE;
@@ -108,10 +75,6 @@ function _addresshistory_civix_civicrm_config(&$config = NULL) {
 
 /**
  * (Delegated) Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
  */
 function _addresshistory_civix_civicrm_xmlMenu(&$files) {
   foreach (_addresshistory_civix_glob(__DIR__ . '/xml/Menu/*.xml') as $file) {
@@ -121,8 +84,6 @@ function _addresshistory_civix_civicrm_xmlMenu(&$files) {
 
 /**
  * Implements hook_civicrm_install().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function _addresshistory_civix_civicrm_install() {
   _addresshistory_civix_civicrm_config();
@@ -133,8 +94,6 @@ function _addresshistory_civix_civicrm_install() {
 
 /**
  * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
  */
 function _addresshistory_civix_civicrm_postInstall() {
   _addresshistory_civix_civicrm_config();
@@ -147,8 +106,6 @@ function _addresshistory_civix_civicrm_postInstall() {
 
 /**
  * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
 function _addresshistory_civix_civicrm_uninstall() {
   _addresshistory_civix_civicrm_config();
@@ -159,8 +116,6 @@ function _addresshistory_civix_civicrm_uninstall() {
 
 /**
  * (Delegated) Implements hook_civicrm_enable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function _addresshistory_civix_civicrm_enable() {
   _addresshistory_civix_civicrm_config();
@@ -173,8 +128,6 @@ function _addresshistory_civix_civicrm_enable() {
 
 /**
  * (Delegated) Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  */
 function _addresshistory_civix_civicrm_disable() {
   _addresshistory_civix_civicrm_config();
@@ -187,14 +140,6 @@ function _addresshistory_civix_civicrm_disable() {
 
 /**
  * (Delegated) Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
  */
 function _addresshistory_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   if ($upgrader = _addresshistory_civix_upgrader()) {
@@ -216,14 +161,6 @@ function _addresshistory_civix_upgrader() {
 
 /**
  * Search directory tree for files which match a glob pattern.
- *
- * Note: Dot-directories (like "..", ".git", or ".svn") will be ignored.
- * Note: In Civi 4.3+, delegate to CRM_Utils_File::findFiles()
- *
- * @param string $dir base directory
- * @param string $pattern , glob pattern, eg "*.txt"
- *
- * @return array(string)
  */
 function _addresshistory_civix_find_files($dir, $pattern) {
   if (is_callable(['CRM_Utils_File', 'findFiles'])) {
@@ -243,6 +180,7 @@ function _addresshistory_civix_find_files($dir, $pattern) {
       while (FALSE !== ($entry = readdir($dh))) {
         $path = $subdir . DIRECTORY_SEPARATOR . $entry;
         if ($entry[0] == '.') {
+          // Skip dot files
         }
         elseif (is_dir($path)) {
           $todos[] = $path;
@@ -256,10 +194,6 @@ function _addresshistory_civix_find_files($dir, $pattern) {
 
 /**
  * (Delegated) Implements hook_civicrm_managed().
- *
- * Find any *.mgd.php files, merge their content, and return.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
  */
 function _addresshistory_civix_civicrm_managed(&$entities) {
   $mgdFiles = _addresshistory_civix_find_files(__DIR__, '*.mgd.php');
@@ -280,12 +214,6 @@ function _addresshistory_civix_civicrm_managed(&$entities) {
 
 /**
  * (Delegated) Implements hook_civicrm_caseTypes().
- *
- * Find any and return any files matching "xml/case/*.xml"
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
  */
 function _addresshistory_civix_civicrm_caseTypes(&$caseTypes) {
   if (!is_dir(__DIR__ . '/xml/case')) {
@@ -308,12 +236,6 @@ function _addresshistory_civix_civicrm_caseTypes(&$caseTypes) {
 
 /**
  * (Delegated) Implements hook_civicrm_angularModules().
- *
- * Find any and return any files matching "ang/*.ang.php"
- *
- * Note: This hook only runs in CiviCRM 4.5+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
  */
 function _addresshistory_civix_civicrm_angularModules(&$angularModules) {
   if (!is_dir(__DIR__ . '/ang')) {
@@ -333,8 +255,6 @@ function _addresshistory_civix_civicrm_angularModules(&$angularModules) {
 
 /**
  * (Delegated) Implements hook_civicrm_themes().
- *
- * Find any and return any files matching "*.theme.php"
  */
 function _addresshistory_civix_civicrm_themes(&$themes) {
   $files = _addresshistory_civix_glob(__DIR__ . '/*.theme.php');
@@ -352,16 +272,6 @@ function _addresshistory_civix_civicrm_themes(&$themes) {
 
 /**
  * Glob wrapper which is guaranteed to return an array.
- *
- * The documentation for glob() says, "On some systems it is impossible to
- * distinguish between empty match and an error." Anecdotally, the return
- * result for an empty match is sometimes array() and sometimes FALSE.
- * This wrapper provides consistency.
- *
- * @link http://php.net/glob
- * @param string $pattern
- *
- * @return array, possibly empty
  */
 function _addresshistory_civix_glob($pattern) {
   $result = glob($pattern);
@@ -370,12 +280,6 @@ function _addresshistory_civix_glob($pattern) {
 
 /**
  * Inserts a navigation menu item at a given place in the hierarchy.
- *
- * @param array $menu - menu hierarchy
- * @param string $path - path to parent of this item, e.g. 'my_extension/submenu'
- *    'Mailing', or 'Administer/System Settings'
- * @param array $item - the item to insert (parent/child attributes will be
- *    filled for you)
  */
 function _addresshistory_civix_insert_navigation_menu(&$menu, $path, $item) {
   // If we are done going down the path, insert menu
@@ -416,8 +320,6 @@ function _addresshistory_civix_civicrm_navigationMenu(&$menu) {
 
 /**
  * (Delegated) Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
  */
 function _addresshistory_civix_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   $settingsDir = __DIR__ . DIRECTORY_SEPARATOR . 'settings';
@@ -428,10 +330,6 @@ function _addresshistory_civix_civicrm_alterSettingsFolders(&$metaDataFolders = 
 
 /**
  * (Delegated) Implements hook_civicrm_entityTypes().
- *
- * Find any *.entityType.php files, merge their content, and return.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  */
 function _addresshistory_civix_civicrm_entityTypes(&$entityTypes) {
   $entityFiles = _addresshistory_civix_find_files(__DIR__, '*.entityType.php');
